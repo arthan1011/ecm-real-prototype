0fbcd8e8f08759cbb33cc9de5489f74633ec9b05
arthan1011 <arthan1011@gmail.com>
2015-10-09 18:23:05 +0300

    new IDs added to clientFolders array

--- src/main/webapp/js/properties.js
+++ src/main/webapp/js/properties.js
@@ -5,7 +5,11 @@ function getProperties() {
     accessTemplateName: 'demo',
     clientFolders: [
         "{502C4C50-0000-CF17-80A9-0A2896BCB252}",
-        "{802C4C50-0000-CE1C-ACA3-2CA1B17009DA}"
+        "{802C4C50-0000-CE1C-ACA3-2CA1B17009DA}",
+        "{EA31DBDF-537C-CD53-8676-50226ED00000}",
+        "{417DADE0-6DEE-C538-85BB-50226FF00000}",
+        "{EABA54F7-D936-C0D1-85F3-5022A9E00000}",
+        "{9FC60D8A-0C49-C4E7-86DA-5022A9400000}"
     ]
   }
 }
